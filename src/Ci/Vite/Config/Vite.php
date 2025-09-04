b2769564c16782fe5cf84146fbbab890785de0ef
Codeblaze Admin <rahulksharma199r@gmail.com>
2025-09-04 12:14:10 +0530

    Adjusted some code

--- src/Ci/Vite/Config/Vite.php
+++ src/Ci/Vite/Config/Vite.php
@@ -14,4 +14,20 @@ class Vite extends Config {
 	public ?string $viteConfigPath = ROOTPATH;
 
 	public ?string $viteManifestPath = null;
+
+	/**
+	 * Compatibility for Codeigniter4 cache
+	 * @param  array  $an_array
+	 * @return Vite          
+	 */
+	public static function __set_state(array $an_array): object
+    {
+        $obj = new static();
+        foreach ($an_array as $key => $value) {
+            if (property_exists($obj, $key)) {
+                $obj->$key = $value;
+            }
+        }
+        return $obj;
+    }
 }
